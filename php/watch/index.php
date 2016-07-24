c4e27912a03adba27429f184af973d983955f65a
abbyfizzz <vasanth.amias@gmail.com>
2016-07-24 20:47:55 +0530

    wrapup with php

---
+++ php/watch/index.php
@@ -0,0 +1,18 @@
+<?php
+//this is simple php file to get the url of the youtube
+
+
+?>
+<html>
+<head>
+<title>Welcome Download your youtube video here-</title>
+</head>
+<body>
+<form><?php echo $_POST["v"]; ?>
+<input name="url" type="text">
+<input type="submit" value="Get It :)">
+
+</form>
+</body>
+</html>
+
